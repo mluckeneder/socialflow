a2fda45df3b54780d2a4c1922dc34eba6f22d408
Michael Luckeneder <michael@luckeneder.net>
2013-12-25 09:07:46 -0800

    fb test route

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,19 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import com.restfb._
+import com.restfb.types._
+
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def testFacebook = Action {
+    val publicClient = new DefaultFacebookClient
+    val returnit = publicClient.fetchObject("cocacola", classOf[Page])
+    Ok(returnit.getAbout)
   }
 
 }
